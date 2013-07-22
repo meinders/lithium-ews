29d014a94eb8ca3050c2e7eb21f067071413d252
Gerrit Meinders <meinders1337@gmail.com>
2013-07-22 20:42:14 +0200

    Added minimal test case for EWS writer.

--- src/test/resources/lithium/io/ews/TestEwsWriter.java
+++ src/test/resources/lithium/io/ews/TestEwsWriter.java
@@ -17,17 +17,43 @@
 
 package lithium.io.ews;
 
+import java.io.*;
+import java.nio.*;
+import java.nio.charset.*;
+
+import junit.framework.*;
+
 /**
- * FIXME Need comment
+ * Test case for {@link EwsWriter}.
  *
  * @author Gerrit Meinders
  */
 public class TestEwsWriter
+extends TestCase
 {
 	/**
-	 * Constructs a new instance.
+	 * Tests that a schedule with some songs in it can be read and then written,
+	 * creating a duplicate of the original.
 	 */
-	public TestEwsWriter()
+	public void testSongs()
+	throws IOException
 	{
+		final byte[] scheduleFile = Tools.loadResource( getClass(), "lorem.ews" );
+
+		final EwsParser parser = new EwsParser();
+		parser.setCharset( Charset.forName( "windows-1252" ) );
+		final Schedule schedule = parser.parse( ByteBuffer.wrap( scheduleFile ) );
+
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		final EwsWriter writer = new EwsWriter( out );
+		writer.setCharset( Charset.forName( "windows-1252" ) );
+		writer.write( schedule );
+		final byte[] output = out.toByteArray();
+
+		// TODO: It's not an exact match yet, due to a timestamp rounding issue and slightly different compression of the content.
+//		assertEquals( "Unexpected output.", Arrays.toString( scheduleFile ), Arrays.toString( output ) );
+
+		// Reading back the output should be enough verification for now:
+		parser.parse( ByteBuffer.wrap( output ) );
 	}
 }
